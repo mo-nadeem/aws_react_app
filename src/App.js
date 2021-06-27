944247d011ebfa116046246e7696bc0cb3a9065b
Mohd Nadeem <nadeem.saifi77@gmail.com>
2021-06-27 21:22:23 +0530

    update data

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hii, I'm Mohd Faheem</h1>
+      <h1>Hello World React Application</h1>
+      <h2>A complete log of this run can be found in</h2>
+      <p>This is probably not a problem with npm. There is likely additional logging output above.</p>
     </div>
   );
 }
