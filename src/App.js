ba350d52a9bcfa3f05158db4694502527637dcbe
mohd-nadeem-123 <nadeem@opoyi.com>
2022-12-01 14:39:54 +0530

    add iamge

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World React ORISERV PVT LTD</h1>
-      <h2>Content</h2>
-      <p>Meet Ori, A Self-learning, Intelligent Conversationalist created by the visionaries at ORI, that improves
-conversions & conversations for brands across Industry.</p>
+      <h1>ORISERV PVT LTD</h1>
+      <p>Meet Ori, A Self-learning, Intelligent Conversationalist created by the visionaries at ORI, that improves</p>
+      <img src='oriserv_image.jpg' />
     </div>
   );
 }
