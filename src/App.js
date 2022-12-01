d038cb3e3a6c2f1775ecd22ac8c5c2298dd3c42f
mohd-nadeem-123 <nadeem@opoyi.com>
2022-12-01 11:18:23 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World React ORISERV PVT LTD</h1>
-      <h2>A complete log of this run can be found </h2>
-      <p>This is probably not a problem with npm. There is likely additional logging output above.</p>
+      <h2>Content</h2>
+      <p>Meet Ori, A Self-learning, Intelligent Conversationalist created by the visionaries at ORI, that improves
+conversions & conversations for brands across Industry.</p>
     </div>
   );
 }
