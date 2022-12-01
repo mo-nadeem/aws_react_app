c3766b7318fbe8661610006d91fec2b4cb286797
mohd-nadeem-123 <nadeem@opoyi.com>
2022-12-01 11:15:46 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World React Mohd Nadeem</h1>
-      <h2>A complete log of this run can be found in</h2>
+      <h1>Hello World React ORISERV PVT LTD</h1>
+      <h2>A complete log of this run can be found </h2>
       <p>This is probably not a problem with npm. There is likely additional logging output above.</p>
     </div>
   );
